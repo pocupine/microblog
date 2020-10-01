2adcd4c322ea8b8d9fd406e0a6b9777789c5f3b7
pocupine <yehaojie@gmail.com>
2020-10-02 07:08:47 +1000

    new

--- app/models.py
+++ app/models.py
@@ -22,6 +22,8 @@ class User(UserMixin, db.Model):
     username=db.Column(db.String(64),index=True,unique=True)
     email=db.Column(db.String(64),index=True,unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime,default=datetime.utcnow)
     posts=db.relationship('Post',backref='author',lazy='dynamic')
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
